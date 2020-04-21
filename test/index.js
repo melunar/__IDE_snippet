348742cd41c8c34255bfff22bd30d51ee52cc363
haoyong <yong.hao@turingsenseai.com>
2020-04-21 13:25:14 +0800

    update

--- test/index.js
+++ test/index.js
@@ -3,3 +3,62 @@ console.log('abc');
 for(let i = 0; i < 1; i++) {
   
 }
+console.log('a')
+<template>
+  <div class=''>
+    div.main
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  components: {},
+  mixins: [],
+  props: {
+    //name: { type: Array, default: () => [] }
+  },
+  data: function() {
+    return {}
+  },
+  computed: {},
+  created: function() {},
+  mounted: function() {},
+  beforeDestroy: function() {},
+  watch: {},
+  methods: {}
+}
+</script>
+<style scoped lang='scss'>
+//@import '../less/variables.less'
+//@import '../less/sprite.less'
+. {}
+</style>
+<template>
+  <div class=''>
+    div.main
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  components: {},
+  mixins: [],
+  props: {
+    //name: { type: Array, default: () => [] }
+  },
+  data: function() {
+    return {}
+  },
+  computed: {},
+  created: function() {},
+  mounted: function() {},
+  beforeDestroy: function() {},
+  watch: {},
+  methods: {}
+}
+</script>
+<style scoped lang='scss'>
+//@import '../less/variables.less'
+//@import '../less/sprite.less'
+. {}
+</style>
